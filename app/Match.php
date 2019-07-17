9bd8db165aa39eae45a3bd95b717a299de59f4a0
anitaines <49281422+anitaines@users.noreply.github.com>
2019-07-17 15:14:05 -0300

    Update Match.php

--- app/Match.php
+++ app/Match.php
@@ -3,10 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Match extends Model
 {
   public $guarded = [];
 
-  use softDeletes;
+  use SoftDeletes;
 }
