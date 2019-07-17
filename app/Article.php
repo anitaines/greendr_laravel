8968ff884bb79177d82cea3c0ceb0e397bb5a26d
anitaines <49281422+anitaines@users.noreply.github.com>
2019-07-17 15:13:12 -0300

    Update Article.php

--- app/Article.php
+++ app/Article.php
@@ -3,10 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Article extends Model
 {
   public $guarded = [];
 
-  use softDeletes;
+  use SoftDeletes;
 }
